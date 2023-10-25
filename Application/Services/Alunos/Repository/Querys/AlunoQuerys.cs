298e8ca9c7ecd980f7d210bc5e61e4aa4d06613b
Eros Karsburg Dias da Rosa <eros.rosa@rodosoft.com.br>
2023-10-25 08:14:32 -0300

    feature: in progress database structure

---
+++ Application/Services/Alunos/Repository/Querys/AlunoQuerys.cs
@@ -0,0 +1,17 @@
+﻿using Application.Shared.Entities;
+using System.Configuration;
+using System.Data;
+
+namespace Application.Services.Alunos.Repository.Querys
+{
+    internal class AlunoQuerys
+    {
+        public List<Aluno> SelectAlunos()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            
+
+            return alunos;
+        }
+    }
+}
